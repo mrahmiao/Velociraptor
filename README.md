f8e630f38f6344ae232f58d77a82c25c7addad83
mrahmiao <mrahmiao@gmail.com>
2015-04-20 21:36:15 +0800

    Initial commit
    Signed-off-by: mrahmiao <mrahmiao@gmail.com>

---
+++ README.md
@@ -0,0 +1,7 @@
+# Velociraptor
+
+Velociraptor is a network stubbing framework written in pure Swift for iOS and OS X.
+
+# License
+
+Velociraptor is released under the MIT license. See the `LICENSE` file for details.
---
+++ Velociraptor/Velociraptor.h
@@ -0,0 +1,19 @@
+//
+//  Velociraptor.h
+//  Velociraptor
+//
+//  Created by mrahmiao on 4/20/15.
+//  Copyright (c) 2015 Code4Blues. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Velociraptor.
+FOUNDATION_EXPORT double VelociraptorVersionNumber;
+
+//! Project version string for Velociraptor.
+FOUNDATION_EXPORT const unsigned char VelociraptorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Velociraptor/PublicHeader.h>
+
+
